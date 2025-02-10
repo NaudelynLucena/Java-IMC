fe4a17e7aadd8d21e167ce518fed01324c259119
NaudelynLucena <naulunau@gmail.com>
2025-02-10 21:51:15 +0100

    Implementation of IMCCalculadora. CalcularIMC test passes

--- src/main/java/dev/nau/java_imc/IMCCalculadora.java
+++ src/main/java/dev/nau/java_imc/IMCCalculadora.java
@@ -1,5 +1,15 @@
 package dev.nau.java_imc;
 
 public class IMCCalculadora {
+    private double peso;
+        private double estatura;
 
+        public IMCCalculadora(double peso, double estatura) {
+            this.peso = peso;
+            this.estatura = estatura;
+        }
+
+        public double calcularIMC() {
+            return peso / (estatura * estatura);
+        }
 }
